23a484793608f4eadbe2c5a49be718824afa158d
Andressa <andressacamillo17@gmail.comc>
2023-03-17 13:57:17 -0300

    Aula4

---
+++ Aula4/src/TabuadaV2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TabuadaV2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+
+		int numero, contador = 1;
+
+		System.out.println("Mostre a tabuada do nÃºmero: ");
+		numero = leia.nextInt();
+		
+		while(contador <= 10) {
+			System.out.println(numero + " x " + contador + " = " + (numero * contador));
+			contador ++;
+		}
+
+	}
+
+}
