062ef087994904f78a57b7f699b3c22bed2f1ec7
Andressa <andressacamillo17@gmail.comc>
2023-03-22 23:10:38 -0300

    Aula5

--- Aula01/src/aula01/ifelse.java
+++ Aula01/src/aula01/ifelse.java
@@ -5,6 +5,14 @@ public class ifelse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int estoque =50;
+		
+		if(estoque >=100) {
+			System.out.println("Produto suficiente");
+		}else if(estoque <100 && estoque > 50 ) {
+			System.out.println("Alerta: avaliar possibilidade de novo pedido");
+		}else{
+		System.out.println("Atenção: faça um novo pedido");
 	}
-
+	}
 }
