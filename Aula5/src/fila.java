dc6cfb4b997371d980b611c4a9dc69efcd276041
Andressa <andressacamillo17@gmail.comc>
2023-03-22 23:40:34 -0300

    Aula5

---
+++ Aula5/src/fila.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class fila {
+
+	public static void main(String[] args) {
+Queue<String> fila = new LinkedList<String>();
+		
+		fila.add("Brenda");
+		fila.add("Guilherme");
+		fila.add("Michelle");
+		fila.add("Gabriel");
+		fila.add("Katarina");
+		
+		for(var elemento : fila)
+			System.out.println(elemento);
+			
+			System.out.println("Retirar elemento: ");
+			fila.poll();
+			System.out.println(fila);
+			
+			fila.add("Reynaldo");
+			System.out.println(fila);
+			
+			System.out.println(fila.size());
+			
+			System.out.println(fila.contains("Reynaldo"));
+
+	
+
+	}
+
+}
